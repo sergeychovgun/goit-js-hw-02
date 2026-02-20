f745f3d5bddc0505f03cf1e28a8bda8a5417e09b
serhii <chovgunsergey@gmail.com>
2026-02-20 15:24:50 +0200

    v1

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,14 @@
-function getElementWidth(content, padding, border) {
-  const contentValue = Number.parseFloat(content);
-  const paddingValue = Number.parseFloat(padding);
-  const borderValue = Number.parseFloat(border);
-
-  return contentValue + paddingValue * 2 + borderValue * 2;
+function checkForSpam(message) {
+  let lowMessage = message.toLowerCase();
+  if (lowMessage.includes("sale") || lowMessage.includes("spam")) {
+    return true;
+  }  return false;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
