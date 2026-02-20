37bf2cb103cdfc2c29be581de8ff8ac1243c4ed3
Sergey <137205761+sergeychovgun@users.noreply.github.com>
2026-02-20 15:42:16 +0200

    Remove unnecessary break statements in shipping cost function

--- js/task-4.js
+++ js/task-4.js
@@ -1,19 +1,19 @@
 function getShippingCost(country) {
     switch (country) {
         case "China":
-            return "Shipping to China will cost 100 credits"
-            break;
+            return "Shipping to China will cost 100 credits";
+            
     case "Chile":
-            return "Shipping to Chile will cost 250 credits"
-            break;
+            return "Shipping to Chile will cost 250 credits";
+            
             case "Australia":
-            return "Shipping to Australia will cost 170 credits"
-            break;
+            return "Shipping to Australia will cost 170 credits";
+            
             case "Jamaica":
-            return "Shipping to Jamaica will cost 120 credits"
-            break;
+            return "Shipping to Jamaica will cost 120 credits";
+            
         default:
-            return "Sorry, there is no delivery to your country"
+            return "Sorry, there is no delivery to your country";
     }
 }
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
